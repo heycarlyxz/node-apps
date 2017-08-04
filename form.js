772baf152ac7896157840a1592d962c83690f8b2
heycarlyxz <carlcarreon05@gmail.com>
2017-08-04 11:16:29 +0800

    Closed

--- form.js
+++ form.js
@@ -1,3 +1,24 @@
-var submit = function(){
-  alert("HELLO")
-}
+var events = require('events');
+var util = require('util');
+
+var User = function(name){
+  this.name = name;
+};
+
+util.inherits(User, events.EventEmiiter);
+
+var carl = new User('carl');
+var tin = new User('tin');
+var jm = new User('jm');
+
+var data = [carl, tin, jm];
+
+/*
+data.forEach(function(user){
+  user.on('register',function(type){
+    console.log(user.name + ' has been registered as ' +type);
+  });
+});
+
+carl.emit('register','admin');
+*/
--- models/User.js
+++
@@ -1,8 +0,0 @@
-var username, password;
-
-var User = function(){
-  this.username = username;
-  this.password = password;
-};
-
-module.exports = User;
